5718538a91a2a7fb64f0899723ba41021231673e
Juan Mercado <juan.mercado1986@gmail.com>
2024-03-07 22:26:27 -0300

    Se agrego el endpoint de obtener producto Random

--- src/main/java/com/backend/integrador/repository/IProductoRepository.java
+++ src/main/java/com/backend/integrador/repository/IProductoRepository.java
@@ -1,11 +1,15 @@
 package com.backend.integrador.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.backend.integrador.entity.Producto;
 
 @Repository
 public interface IProductoRepository extends JpaRepository<Producto, Long>{
-    
+    @Query(value = "SELECT * FROM producto ORDER BY RAND() LIMIT 2", nativeQuery = true)
+    List<Producto> obtenerProductosAleatorios();
 }
