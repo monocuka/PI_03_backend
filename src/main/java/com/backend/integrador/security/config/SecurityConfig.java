86d3c789b1520ac15a12e876a2961f87fd83be99
Juan Mercado <juan.mercado1986@gmail.com>
2024-03-14 23:13:11 -0300

    Se configuro Cors y de deshabilito el csrf

--- src/main/java/com/backend/integrador/security/config/SecurityConfig.java
+++ src/main/java/com/backend/integrador/security/config/SecurityConfig.java
@@ -1,17 +1,23 @@
 package com.backend.integrador.security.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.OrRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.backend.integrador.security.filter.JWTFilter;
 
@@ -29,9 +35,11 @@ public class SecurityConfig {
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		//http.csrf(AbstractHttpConfigurer::disable);
 		//http.csrf(csrf->csrf.disable());
-		http.authorizeHttpRequests(auth->auth
-					.requestMatchers(getPublicEndpoints()).permitAll()
-					.anyRequest().authenticated())
+		http.cors( cors -> corsConfigurationSource())
+			.csrf( AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(auth->auth
+			.requestMatchers(getPublicEndpoints()).permitAll()
+			.anyRequest().authenticated())
 			.sessionManagement(sess->sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authenticationProvider(authenticationProvider)
 			.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
@@ -52,5 +60,18 @@ public class SecurityConfig {
 			new AntPathRequestMatcher("**")
 			);
 	}
+
+		@Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(List.of("Authorization", "Cache-control", "Content-type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }
 
